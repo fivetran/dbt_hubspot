04ef3f1f7855d7ed746f298f825b18a47f0c0afa
Dylan Baker <dylang.baker@googlemail.com>
2020-06-15 13:09:17 -0400

    add email metrics to sends

--- models/hubspot__email_sends.sql
+++ models/hubspot__email_sends.sql
@@ -57,11 +57,44 @@ with sends as (
 
     select
         sends.*,
-        coalesce(bounces.bounces,0) as bounces
+        coalesce(bounces.bounces,0) as bounces,
+        coalesce(clicks.clicks,0) as clicks,
+        coalesce(deferrals.deferrals,0) as deferrals,
+        coalesce(deliveries.deliveries,0) as deliveries,
+        coalesce(drops.drops,0) as drops,
+        coalesce(forwards.forwards,0) as forwards,
+        coalesce(opens.opens,0) as opens,
+        coalesce(prints.prints,0) as prints,
+        coalesce(spam_reports.spam_reports,0) as spam_reports,
+        coalesce(unsubscribes.unsubscribes,0) as unsubscribes   
     from sends
     left join bounces using (email_send_id)
+    left join clicks using (email_send_id)
+    left join deferrals using (email_send_id)
+    left join deliveries using (email_send_id)
+    left join drops using (email_send_id)
+    left join forwards using (email_send_id)
+    left join opens using (email_send_id)
+    left join prints using (email_send_id)
+    left join spam_reports using (email_send_id)
+    left join unsubscribes using (email_send_id)
+
+), booleans as (
+
+    select 
+        *,
+        bounces > 0 as was_bounced,
+        clicks > 0 as was_clicked,
+        deferrals > 0 as was_deffered,
+        deliveries > 0 as was_delivered,
+        forwards > 0 as was_forwarded,
+        opens > 0 as was_opened,
+        prints > 0 as was_printed,
+        spam_reports > 0 as was_spam_reported,
+        unsubscribes > 0 as was_unsubcribed
+    from joined
 
 )
 
 select *
-from joined
+from booleans
