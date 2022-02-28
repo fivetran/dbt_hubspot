d4cbb421c307c9077baba26ffe047007b5ca118e
Matt Lee <matt_lee07@live.ca>
2022-02-28 14:53:49 -0500

    add if enabled vars statement to select.

--- macros/engagements_joined.sql
+++ macros/engagements_joined.sql
@@ -14,13 +14,13 @@ with base as (
 
     select 
         base.*,
+        {% if fivetran_utils.enabled_vars(['hubspot_engagement_contact_enabled']) %} engagements.contact_ids, {% endif %}
+        {% if fivetran_utils.enabled_vars(['hubspot_engagement_deal_enabled']) %} engagements.deal_ids, {% endif %}
+        {% if fivetran_utils.enabled_vars(['hubspot_engagement_company_enabled']) %} engagements.company_ids, {% endif %}
         engagements.is_active,
         engagements.created_timestamp,
         engagements.occurred_timestamp,
-        engagements.owner_id,
-        engagements.contact_ids,
-        engagements.deal_ids,
-        engagements.company_ids
+        engagements.owner_id
     from base
     left join engagements
         using (engagement_id)
