46b45e2f021f62bdc524b2592c99ee4904102a24
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-04-28 00:07:37 -0500

    regen docs

--- macros/adjust_email_metrics.sql
+++ macros/adjust_email_metrics.sql
@@ -1,16 +1,13 @@
 {% macro adjust_email_metrics(base_ref, var_name) %}
 
-{% set base_cols = adapter.get_columns_in_relation(ref(base_ref))|map(attribute='name')|map('lower')|list %}
-{% set email_metrics = var(var_name) %}
+{% set base_cols = dbt_utils.get_filtered_columns_in_relation(ref(base_ref))|map('lower') %}
 
-{# Remove metrics not in base #}
-{% set new_cols = [] %}
-{% for metric in email_metrics %}
-    {% if metric|lower in base_cols %}
-        {% do new_cols.append(metric) %}
-    {% endif %}
-{% endfor %}
+{% set default_metrics = ['bounces', 'clicks', 'deferrals', 'deliveries', 'drops', 'forwards', 'opens', 'prints', 'spam_reports', 'unsubscribes'] %}
+{% set email_metrics = var(var_name, default_metrics)|map('lower') %}
 
-{{ return(new_cols) }}
+{# Only keep metrics from the base ref #}
+{% set adjusted_cols = email_metrics|select('in', base_cols)|list %}
+
+{{ return(adjusted_cols) }}
 
 {% endmacro %}
