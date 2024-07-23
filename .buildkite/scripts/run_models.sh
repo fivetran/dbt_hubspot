814f1a535ab737cbab9199e4aecbb0c56bb7b0e2
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-23 17:41:58 -0500

    update run_models

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -21,7 +21,7 @@ dbt seed --vars '{hubspot_schema: hubspot_sqlw_tests}' --target "$db" --full-ref
 dbt compile --vars '{hubspot_schema: hubspot_sqlw_tests}' --target "$db"
 dbt run --vars '{hubspot_schema: hubspot_sqlw_tests}' --target "$db" --full-refresh
 dbt test --vars '{hubspot_schema: hubspot_sqlw_tests}' --target "$db"
-dbt run --vars '{hubspot_schema: hubspot_sqlw_tests, hubspot_marketing_enabled: true, hubspot_contact_merge_audit_enabled: true, hubspot_sales_enabled: false}}' --target "$db"
+dbt run --vars '{hubspot_schema: hubspot_sqlw_tests, hubspot_marketing_enabled: true, hubspot_contact_merge_audit_enabled: true, hubspot_sales_enabled: false}' --target "$db"
 dbt run --vars '{hubspot_schema: hubspot_sqlw_tests, hubspot_marketing_enabled: false, hubspot_sales_enabled: true, hubspot_merged_deal_enabled: true, hubspot__pass_through_all_columns: true, hubspot_using_all_email_events: false, hubspot_owner_enabled: false}' --target "$db"
 else
 dbt seed --target "$db" --full-refresh
