4a1a00c10785ce652aa29102a801a67554b260ea
Avinash Kunnath <avinash.kunnath@fivetran.com>
2026-01-29 11:12:30 -0800

    field order update

--- macros/engagements_joined.sql
+++ macros/engagements_joined.sql
@@ -17,10 +17,10 @@ with base as (
         {% if fivetran_utils.enabled_vars(['hubspot_engagement_contact_enabled']) %} engagements.contact_ids, {% endif %}
         {% if fivetran_utils.enabled_vars(['hubspot_engagement_deal_enabled']) %} engagements.deal_ids, {% endif %}
         {% if fivetran_utils.enabled_vars(['hubspot_engagement_company_enabled']) %} engagements.company_ids, {% endif %}
+        not base._fivetran_deleted as is_active,
         base.created_timestamp,
         base.occurred_timestamp,
-        base.owner_id,
-        not base._fivetran_deleted as is_active
+        base.owner_id
     from base
     left join engagements
         on base.engagement_id = engagements.engagement_id
